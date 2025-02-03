2dd33f6bea7e5bbe0dd6859c2bcd344ce90fb327
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-02-03 22:10:21 +0000

    published migration and also added the relationsip between task and user

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,5 +9,9 @@ class Task extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'description', 'price', 'completed', ''];
+    protected $fillable = ['name', 'description', 'price', 'completed'];
+
+    public function user() {
+        $this->belongsTo(User::class);
+    }
 }
