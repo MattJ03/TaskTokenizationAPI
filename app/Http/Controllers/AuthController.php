9e311ac0fabdccc83674f878f62b111deb862da3
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-02-04 20:29:00 +0000

    logout method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,6 +23,27 @@ class AuthController extends Controller
         ]);
 
         $token = $user->createToken('token')->plainTextToken;
+        return response()->json(['user' => $user, 'token' => $token],201);
 
+    }
+
+    public function login(Request $request) {
+        $request->validate([
+            'name' => 'required|string|max:50',
+            'email' => 'required|string|email|max:50',
+            'password' => 'required|string|min:6|max:25',
+        ]);
+         //make sure the user logged in under correct creds
+        if(!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['Message' => 'Wrongs Credentials'], 401);
+        }
+        $user = Auth::user();
+        $token = $user->createToken('token')->plainTextToken;
+        return response()->json(['user' => $user, 'token' => $token], 201);
+    }
+
+    public function logout(Request $request)  {
+        $request->user()->tokens()->delete();
+        return response()->json(['Message' => 'Logged out'], 201);
     }
 }
